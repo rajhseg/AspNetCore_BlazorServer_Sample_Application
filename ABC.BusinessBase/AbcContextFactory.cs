2ca05551f023fed067410fc626ba915d514b5009
Rajesh <rajhseg@gmail.com>
2024-03-17 07:39:31 +0530

    dotnet ef db create
    dotnet ef db create

---
+++ ABC.BusinessBase/AbcContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ABC.BusinessBase;
+
+public class AbcContextFactory : IDesignTimeDbContextFactory<AbcContext>
+{
+    public AbcContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AbcContext>();
+        optionsBuilder.UseSqlServer<AbcContext>("server=systemname\\sql;database=test;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+        return new AbcContext(optionsBuilder.Options);
+    }
+}
